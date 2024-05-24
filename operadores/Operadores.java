49c7526cfd49bb8e3720defefab1d4e4a800a843
Rafaely RP <rafarprocopio@gmail.com>
2024-05-24 15:35:26 -0300

    alterações da aula de operadores lógicos

--- operadores/Operadores.java
+++ operadores/Operadores.java
@@ -32,7 +32,11 @@ public class Operadores {
        
         boolean condicao1=true;
 
-        boolean condicao2=false;
+        boolean condicao2=true;
+
+        if(condicao1 && (7 > 4)){
+            System.out.println("as duas condições são verdadeiras");
+        }
         
         if(condicao1 && condicao2){
             System.out.println("as duas condições são verdadeiras");
