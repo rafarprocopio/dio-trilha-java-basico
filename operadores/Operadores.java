507be5321e5dfbcfa69256dcc505776c85c74f2a
Rafaely RP <rafarprocopio@gmail.com>
2024-05-24 15:07:15 -0300

    alterações na aula operadores relacionais

--- operadores/Operadores.java
+++ operadores/Operadores.java
@@ -1,10 +1,36 @@
 public class Operadores {
     public static void main(String[] args) {
+        
+        String nomeUm = "RAFAELY";
+        String nomeDois = "RAFAELY";
+
+        System.out.println(nomeUm == nomeDois);
+        
+            
         int numero1 = 1;
         int numero2 = 2;
-
+        
         boolean simNao = numero1 == numero2;
+        if(numero1 < numero2){
+            System.out.println("a nossa condição é verdadeira");
+        }
 
-        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+        if(numero1 == numero2){
+            System.out.println("a nossa condição é verdadeira");
+        }
+
+        System.out.println("numero Um é igual a numero Dois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numero Um é diferente a numero Dois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numero Um é maior que numero Dois? " + simNao);
+
+
+
+
     }
 }
