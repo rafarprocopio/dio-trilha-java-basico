36540c4e4607e6c067d714d0db2bcf120c7aa91d
Rafaely RP <rafarprocopio@gmail.com>
2024-05-24 13:47:43 -0300

    alteração aula operadores relacionais

--- operadores/Operadores.java
+++ operadores/Operadores.java
@@ -1,10 +1,10 @@
-public class operadores {
+public class Operadores {
     public static void main(String[] args) {
         int numero1 = 1;
         int numero2 = 2;
 
         boolean simNao = numero1 == numero2;
 
-        System.out.println("numeroUm é igual a numeroDois?" + SimNao);
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
     }
 }
