5d7e63cfa452eff9434b581578455a6d7ddffe9e
Rafaely RP <rafarprocopio@gmail.com>
2024-05-24 12:48:09 -0300

    operadores - tipo relacionais

---
+++ tipos-variáveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class operadores {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a numeroDois?" + SimNao);
+    }
+}
