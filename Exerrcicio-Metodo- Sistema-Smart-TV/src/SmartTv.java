ccbb8acfcdf48f86be9b87bc9c71a7ee3215cc19
Rafaely RP <rafarprocopio@gmail.com>
2024-05-25 07:03:08 -0300

    alterações exercício Métodos - propjeto SmartTv

--- Exerrcicio-Metodo- Sistema-Smart-TV/src/SmartTv.java
+++ Exerrcicio-Metodo- Sistema-Smart-TV/src/SmartTv.java
@@ -4,7 +4,21 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
     public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume para : " + volume);
         //volume = volume + 1;
         volume++;
     }
@@ -12,6 +26,8 @@ public class SmartTv {
     public void diminuirVolume(){
         //volume = volume - 1;
         volume--;
+        System.out.println("Diminuindo o volume para : " + volume);
+
     }
 
     public void ligar(){
--- Exerrcicio-Metodo- Sistema-Smart-TV/src/Usuario.java
+++ Exerrcicio-Metodo- Sistema-Smart-TV/src/Usuario.java
@@ -8,6 +8,13 @@ public class Usuario {
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
+
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal Atual : " + smartTv.canal);
+
         System.out.println("Volume Atual : " + smartTv.volume);
 
         System.out.println("Tv Ligada? " + smartTv.ligada);
