7ec806317c689064abe350ef75954ca69753ec80
Rafaely RP <rafarprocopio@gmail.com>
2024-05-28 20:17:08 -0300

    alterações projeto-desafio ContaBancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -8,11 +8,10 @@ public class ContaTerminal {
 
         //Exibir as mensagens para os nossos usuários
 
-        //Obeter pela classe scanner os valores digitados pelo terminal
+        //Obter pela classe scanner os valores digitados pelo terminal
 
         //Exibir a mensagem conta criada
        
-
     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
     System.out.println("Por favor, digite o número da conta");
@@ -21,15 +20,16 @@ public class ContaTerminal {
     System.out.println("Por favor, digite da agência");
         String numeroAgencia = scanner.next();
 
-     System.out.println("Por favor, digite o seu nome");
+    System.out.println("Por favor, digite o seu nome");
         String nomeCliente = scanner.next();
+
+    System.out.println("Por favor, digite o seu sobrenome");
+        String sobrenomeCliente = scanner.next();
 
     System.out.println("Por favor, digite o saldo da conta");
         double saldo = scanner.nextDouble();
 
     //imprimindo os dados obtidos pelo usuario
-    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.1021");
-
-
+    System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
